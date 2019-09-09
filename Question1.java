ac4195cfc11dfeb0d75a2172208d3b17cc927d06
marc-town <asamin.itconsultant@gmail.com>
2019-09-09 17:07:55 +0900

    commit test

--- Question1.java
+++ Question1.java
@@ -1,6 +1,6 @@
 public class Question1 {
 	public static void main (String[] args) {
-		System.out.print("Hello");
-		System.out.println(" World.");
+		System.out.print("私は");
+		System.out.println("平凡な福太郎です");
 	}
 }
